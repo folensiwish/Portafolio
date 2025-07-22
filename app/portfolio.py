af2b6546dd8e9e9580f5d73fd9a2be06f9b0bd91
folensiwish <fer.albertoandres@gmail.com>
2025-07-22 14:36:02 -0400

    Formulario de contacto creado

--- app/portfolio.py
+++ app/portfolio.py
@@ -7,3 +7,8 @@ bp = Blueprint('portfolio', __name__, url_prefix='/')
 @bp.route('/', methods=['GET'])
 def index():
     return render_template('portfolio/index.html')
+
+@bp.route('/contact', methods=['POST'])
+
+def contact():
+    return "lala"
